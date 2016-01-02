6de2d29c0095bf3e817cde936c4d3d649b5b067e
Richard Jansen <richard@1337h4x.nl>
2016-01-02 21:04:55 +0100

    yeah dawg

---
+++ afvinkie3/src/opdracht1.java
@@ -0,0 +1,16 @@
+/**
+ * Created by rich_ on 1/2/2016.
+ */
+import java.util.*;
+
+public class opdracht1 {
+    public static void main(String[] args) {
+        Stack jemoeder = new Stack();
+        Scanner reader = new Scanner(System.in);
+        String a = reader.next();
+
+    }
+    public static Stack checkie(String a){
+
+    }
+}
