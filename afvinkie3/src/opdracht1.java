610c6c912eb7434fe45a19572ce31e2f925f7d28
Richard Jansen <richard@1337h4x.nl>
2016-01-07 23:08:03 +0100

    yeah dawg

--- afvinkie3/src/opdracht1.java
+++ afvinkie3/src/opdracht1.java
@@ -5,12 +5,56 @@ import java.util.*;
 
 public class opdracht1 {
     public static void main(String[] args) {
-        Stack jemoeder = new Stack();
+        Stack meh = new Stack();
         Scanner reader = new Scanner(System.in);
+        System.out.println("Enter Code you Scrub! \n");
         String a = reader.next();
+        int l = a.length();
+        int j = 0;
+        int rondehaak = 0;
+        int hoekshit = 0;
+        int acculadeshit = 0;
+        for (int i = 0; i < l; i++) {
+            j++;
+            if (a.substring(i, j).equals("(")) {
+                rondehaak++;
+            }
+            if (a.substring(i, j).equals(")")) {
+                rondehaak--;
+            }
+            if (a.substring(i, j).equals("[")) {
+                hoekshit++;
+            }
+            if (a.substring(i, j).equals("]")) {
+                hoekshit--;
+            }
+            if (a.substring(i, j).equals("{")) {
+                acculadeshit++;
+            }
+            if (a.substring(i, j).equals("}")) {
+                acculadeshit--;
+            }
 
-    }
-    public static Stack checkie(String a){
+        }
+        if (rondehaak < 0){
+            meh.push(" Te veel ')' ");
+        }
+        if (rondehaak > 0){
+            meh.push(" Te veel '(' ");
+        }
+        if (hoekshit < 0){
+            meh.push(" Te veel ']' ");
+        }
+        if (hoekshit > 0){
+            meh.push(" Te veel '[' ");
+        }
+        if (acculadeshit < 0){
+            meh.push(" Te veel '}' ");
+        }
+        if (acculadeshit > 0){
+            meh.push(" Te veel '{' ");
+        }
+        System.out.println(meh);
 
     }
 }
